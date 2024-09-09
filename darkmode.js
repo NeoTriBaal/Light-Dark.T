6af788bf3db30424e748f4667b61e9d6db6b9f4f
NeoTriBaal <aces.tcgdealer@gmail.com>
2024-09-08 21:51:32 -0400

    Final project dark//light tutorial

---
+++ darkmode.js
@@ -0,0 +1,19 @@
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("theme-switch");
+
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+};
+
+const disableDarkMode = () => {
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+};
+
+if (darkmode === "active") enableDarkMode();
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+});
